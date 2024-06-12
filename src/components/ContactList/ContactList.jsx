7759dd6ed342a09503433f71681e4dfb3e3af5e8
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-06-13 01:26:31 +0300

    enhanced

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,27 @@
-import css from "./ContactList.module.css";
-
-import Contact from "../Contact/Contact";
-
-import { useSelector } from "react-redux";
-
-import { selectVisibleContacts } from "../../redux/contactsSlice";
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSelector } from 'react-redux';
+import { selectVisibleContacts } from '../../redux/contactsSlice';
+import Contact from '../Contact/Contact';
+import css from './ContactList.module.css';
 
 function ContactList() {
   const contacts = useSelector(selectVisibleContacts);
 
-
   return (
-    <ul className={css["contact-list"]}>
-      {contacts.map((contact) => (
-        <li key={contact.id}>
-          <Contact contact={contact} />
-        </li>
-      ))}
+    <ul className={css['contact-list']}>
+      <AnimatePresence>
+        {contacts.map(contact => (
+          <motion.li
+            key={contact.id}
+            initial={{ opacity: 0, scale: 0.75 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.9 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Contact contact={contact} />
+          </motion.li>
+        ))}
+      </AnimatePresence>
     </ul>
   );
 }
